03e7d8f1e6547eea039b06296ac4c15f528a1291
Miquel De Cáceres <miquelcaceres@gmail.com>
2024-05-28 17:51:09 +0200

    column type check

--- R/check_harmonized_table.R
+++ R/check_harmonized_table.R
@@ -4,6 +4,7 @@
 #'
 #' @param x A data frame with harmonized trait data
 #'
+#' @returns An (invisible) boolean indicating whether the data frame is acceptable or not.
 #' @export
 #'
 check_harmonized_table<- function(x) {
@@ -29,8 +30,28 @@ check_harmonized_table<- function(x) {
     if(!(t %in% HarmonizedTraitDefinition$Notation)) {
       acceptable <- FALSE
       cli::cli_alert_warning(paste0("Trait column '", t, "' does not match any harmonized trait."))
+    } else {
+      type <- HarmonizedTraitDefinition$Type[HarmonizedTraitDefinition$Notation == t]
+      if(type=="String") {
+        if(!inherits(x[[t]], "character")) {
+          acceptable <- FALSE
+          cli::cli_alert_warning(paste0("Trait column '", t, "' should contain character data."))
+        }
+      } else if(type =="Integer") {
+        if(!inherits(x[[t]], "integer")) {
+          acceptable <- FALSE
+          cli::cli_alert_warning(paste0("Trait column '", t, "' should contain integer data."))
+        }
+      } else if(type =="Numeric") {
+        if(!inherits(x[[t]], "numeric")) {
+          acceptable <- FALSE
+          cli::cli_alert_warning(paste0("Trait column '", t, "' should contain numeric data."))
+        }
+      }
     }
   }
   if(!acceptable) cli::cli_alert_warning("The data frame is not acceptable as harmonized source data.")
   else  cli::cli_alert_success("The data frame is acceptable as harmonized source data.")
+
+  return(invisible(acceptable))
 }
