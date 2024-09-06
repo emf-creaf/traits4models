16a7ca3290dddd819eaa69ddb1c70a19acd0a632
Miquel De Cáceres <miquelcaceres@gmail.com>
2024-09-06 12:01:28 +0200

    CrownRatio added fill

--- R/fill_harmonized_traits.R
+++ R/fill_harmonized_traits.R
@@ -99,6 +99,17 @@ fill_medfate_traits<-function(SpParams,
                               replace_previous = replace_previous, verbose = verbose)
 
 
+  if(progress) cli::cli_progress_step(paste0("Processing parameter: ", "CR"))
+  trait_table <- get_trait_data(harmonized_trait_path, "CrownRatio", is_numeric = TRUE, progress = FALSE)
+  trait_mapping <- "CrownRatio"
+  names(trait_mapping) <- "CR"
+  SpParams <- populate_traits(SpParams, trait_table, trait_mapping,
+                              summary_function = "median",
+                              taxon_column = "acceptedName", genus_column = "genus",
+                              priority_column = priority_column,
+                              erase_previous = erase_previous, character_traits = FALSE,
+                              replace_previous = replace_previous, verbose = verbose)
+
   if(progress) cli::cli_progress_step(paste0("Processing parameter: ", "Gs_P50"))
   trait_table <- get_trait_data(harmonized_trait_path, "Gs_P50", is_numeric = TRUE, progress = FALSE)
   trait_mapping <- "Gs_P50"
